4e662059132402c4c92ef062658df9c5b1ef0732
Brandon Whittle <brandonwwhittle@gmail.com>
2022-08-04 19:27:28 -0500

    FEAT(): Add skills

--- src/modules/pages/_Skills.tsx
+++ src/modules/pages/_Skills.tsx
@@ -6,26 +6,53 @@ const Skills = (): JSX.Element => {
     <>
       <LinkHeader />
       <PageContainer title="Skills">
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. A nesciunt,
-          et vel reprehenderit similique cupiditate molestias hic culpa illum
-          exercitationem dolorum iusto repellat perspiciatis modi perferendis,
-          voluptatibus at ab fugiat temporibus, debitis error numquam facilis.
-          Quas odio illum qui hic atque expedita dolorem iusto quia. Neque
-          possimus iusto sed laborum sapiente ullam ipsa a optio nostrum autem?
-          Corporis recusandae at delectus! Sed quasi cumque vitae nostrum nisi
-          aliquid enim? Beatae, quo maiores. Recusandae soluta odio aperiam est
-          dignissimos consequuntur, mollitia, optio vitae amet expedita eveniet
-          dolorem, doloribus explicabo quasi aspernatur. Numquam illo recusandae
-          nam voluptatibus blanditiis asperiores earum vero porro quisquam
-          voluptas velit facilis beatae necessitatibus, similique architecto,
-          quibusdam fuga quasi accusamus consectetur. Consequatur a molestiae
-          quo? Maxime consequatur deserunt illo quod quo ducimus quasi obcaecati
-          animi consectetur facere minus labore, voluptates, molestiae natus,
-          earum commodi dolore voluptate. Laboriosam tempora nihil expedita,
-          maiores debitis voluptate odit culpa temporibus nemo est nobis
-          recusandae
-        </p>
+        <div className="skills-content">
+          <div className="skill-section">
+            <div className="hard-skills">
+              <h2>Hard Skills</h2>
+              <ul>
+                <li>TypeScript</li>
+                <li>React</li>
+                <li>Python</li>
+                <li>Go</li>
+                <li>Yarn</li>
+                <li>Linux</li>
+                <li>SolidWorks</li>
+                <li>Adobe Photoshop</li>
+                <li>Adobe Illustrator</li>
+                <li>Microsoft Office Suite</li>
+              </ul>
+            </div>
+          </div>
+          {/* <div className="skill-section">
+            <div className="soft-skills">
+              <h2>Soft Skill</h2>
+              <ul>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+              </ul>
+            </div>
+          </div> */}
+
+          <div className="skill-section">
+            <div className="coursework">
+              <h2>Coursework</h2>
+              <ul>
+                <li>Environmental Chemistry</li>
+                <li>Environmental Biology</li>
+                <li>Environmental Law & Regulations</li>
+                <li>Water & Wastewater Engineering</li>
+                <li>Phytoremediation</li>
+                <li>Thermodynamics</li>
+                <li>Fluid Mechanics</li>
+              </ul>
+            </div>
+          </div>
+        </div>
       </PageContainer>
     </>
   )
